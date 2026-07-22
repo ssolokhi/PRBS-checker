38bbb96a258e306ee291eced19a678b823142487
ssolokhi <solokhin02@gmail.com>
2026-07-22 12:58:03 +0200

    Instantiate PRBS checker FSM on receiving side

--- prbs_checker_top.v
+++ prbs_checker_top.v
@@ -2,7 +2,7 @@ module prbs_checker_top (
     input i_clock,
     output o_led_locked,
     output o_led_error
-);
+    );
     localparam c_PRBS_BITS = 31;
     wire w_prbs_bit;
 
@@ -10,4 +10,12 @@ module prbs_checker_top (
         .i_clock(i_clock),
         .o_last_lfsr_bit(w_prbs_bit)
     );
+
+    prbs_checker #(.c_LOCK_THRESHOLD(c_PRBS_BITS), .c_OPEN_THRESHOLD(c_PRBS_BITS)) prbs_checker (
+        .i_clock(i_clock),
+        .i_received_prbs_bit(w_prbs_bit),
+        .o_is_locked(o_led_locked),
+        .o_error(o_led_error)
+    );
+
 endmodule
