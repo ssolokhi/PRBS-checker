c6a3d84a84490111309b9bf645bd6fbec0feb0e3
ModernPhysics <solokhin02@gmail.com>
2026-08-25 21:49:53 +0200

    Fix width mismatch when comparing thresholds to counters

--- src/prbs_checker.sv
+++ src/prbs_checker.sv
@@ -1,6 +1,13 @@
 `default_nettype none
 
-module prbs_checker #(parameter int c_LOCK_THRESHOLD = 31, parameter int c_OPEN_THRESHOLD = 31) (
+module prbs_checker #(
+    parameter int c_LOCK_THRESHOLD = 31,
+    parameter int c_OPEN_THRESHOLD = 31,
+    // int = 32 bits wide. To compare thresholds with counters without warnings from linter,
+    // thresholds have to be resized to be same width as counters
+    parameter logic [$clog2(c_LOCK_THRESHOLD)-1:0] c_LOCK_THRESHOLD_resized = c_LOCK_THRESHOLD;
+    parameter logic [$clog2(c_LOCK_THRESHOLD)-1:0] c_OPEN_THRESHOLD_resized = c_OPEN_THRESHOLD;
+    ) (
     input logic i_clock,
     input logic i_received_prbs_bit,
     input logic i_expected_prbs_bit,
@@ -23,8 +30,8 @@ module prbs_checker #(parameter int c_LOCK_THRESHOLD = 31, parameter int c_OPEN_
             OPEN:
             begin
                 if (i_received_prbs_bit != i_expected_prbs_bit) lock_counter <= 0; // because correct bits (if any) are not consequtive
-                if (lock_counter < c_LOCK_THRESHOLD-1 && i_received_prbs_bit == i_expected_prbs_bit) lock_counter <= lock_counter + 1;
-                if (lock_counter == c_LOCK_THRESHOLD-1)
+                if (lock_counter < c_LOCK_THRESHOLD_resized-1 && i_received_prbs_bit == i_expected_prbs_bit) lock_counter <= lock_counter + 1;
+                if (lock_counter == c_LOCK_THRESHOLD_resized-1)
                 begin
                     lock_counter <= 0;
                     open_counter <= 0;
@@ -34,12 +41,12 @@ module prbs_checker #(parameter int c_LOCK_THRESHOLD = 31, parameter int c_OPEN_
             LOCKED:
             begin
                 if (i_received_prbs_bit == i_expected_prbs_bit) error <= 1'b0; 
-                if (open_counter < c_OPEN_THRESHOLD-1 && i_received_prbs_bit != i_expected_prbs_bit) 
+                if (open_counter < c_OPEN_THRESHOLD_resized-1 && i_received_prbs_bit != i_expected_prbs_bit) 
                 begin
                     error <= 1'b1;
                     open_counter <= open_counter + 1;
                 end
-                if (open_counter == c_OPEN_THRESHOLD-1)
+                if (open_counter == c_OPEN_THRESHOLD_resized-1)
                 begin
                     open_counter <= 0;
                     lock_counter <= 0;
